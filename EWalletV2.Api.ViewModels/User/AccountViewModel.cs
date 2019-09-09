69c845a28a11cbb747fd1dfe2063f0881368d916
Pop_Chairat <Mr.Chairat_Smart@outlook.com>
2019-09-09 11:31:19 +0700

    Add UserFolder and Add AccountCommand/ViewModel

---
+++ EWalletV2.Api.ViewModels/User/AccountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EWalletV2.Api.ViewModels.User
+{
+    public class AccountViewModel
+    {
+        public string AccountName { get; set; }
+    }
+}
