64f4a8e622821a3e24bc266c4e48cb32a50bdec1
Natthaset <natthaset2538@hotmail.com>
2019-09-09 16:30:13 +0700

    Fixed TransactionEntity

--- Applications/EWalletV2.Domain/Entities/TransactionEntity.cs
+++ Applications/EWalletV2.Domain/Entities/TransactionEntity.cs
@@ -7,8 +7,8 @@ namespace EWalletV2.Domain.Entities
     public class TransactionEntity : BaseEntity
     {
         public string TransactionType { get; set; }
-        public int ReceiveId { get; set; }
-        public int PayerId { get; set; }
+        public int CustomerId { get; set; }
+        public int OtherId { get; set; }
         public decimal Amount { get; set; }
     }
 }
