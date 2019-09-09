6f4748d91971863fb3140d38b221187a32d5db6f
FreePM <622707130011@dpu.ac.th>
2019-09-09 11:28:02 +0700

    Create Payment(Controller Only)

---
+++ Applications/EWalletV2.Domain/Interfaces/ITransactionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EWalletV2.Domain.DtoModels.Transaction;
+
+namespace EWalletV2.Domain.Interfaces
+{
+    public interface ITransactionService
+    {
+        PaymentDto Payment(string email, string merchantAccNo, decimal pay);
+    }
+}
--- EWalletV2.Api/Controllers/TransactionController.cs
+++ EWalletV2.Api/Controllers/TransactionController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EWalletV2.Api.ViewModels;
+using EWalletV2.Api.ViewModels.Transaction;
+using EWalletV2.Domain.DtoModels.Transaction;
+using EWalletV2.Domain.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +15,47 @@ namespace EWalletV2.Api.Controllers
     [ApiController]
     public class TransactionController : ControllerBase
     {
+        private readonly IUserService _userService;
+        private readonly ITransactionService _transactionService;
+        public TransactionController(IUserService userService, ITransactionService transactionService)
+        {
+            _userService = userService;
+            _transactionService = transactionService;
+        }
         //GetTransaction30Days
         //Payment
+        [HttpPost("payment")]
+        public IActionResult Payment([FromBody]PaymentCommand command)
+        {
+            string email = command.Email;
+            string merchantAccNo = command.MerchantAccountNo;
+            decimal pay = command.Pay;
+
+            if (Int32.Parse(merchantAccNo.Substring(0, 2)) != (int)EW_Enumerations.EW_UserTypeEnum.Merchant)
+            {
+                return BadRequest();
+            }
+
+            bool isEmail = _userService.ExistingEmail(email);
+            bool isMerchant = _userService.ExistAccountNo(merchantAccNo);
+
+            if (!isEmail || !isMerchant)
+            {
+                return BadRequest();
+            }
+
+            PaymentDto payment = _transactionService.Payment(email, merchantAccNo, pay);
+
+            PaymentViewModel result = new PaymentViewModel()
+            {
+                Reference = payment.Reference,
+                CreateDatetime = payment.CreateDatetime
+            };
+
+            return Ok(result);
+
+        }
+
         //Topup
         //GetDetailTransaction
         //GenerateTopup
