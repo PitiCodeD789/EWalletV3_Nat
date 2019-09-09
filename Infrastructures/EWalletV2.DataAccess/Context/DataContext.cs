f42f6161d0dff39826d44ee52bfa60cc86bc208a
Terawit Boonyasiri <terawit.boonyasiri@gmail.com>
2019-09-09 14:33:26 +0700

    Add GetUserByEmail
    Add GetUserByEmail

---
+++ Infrastructures/EWalletV2.DataAccess/Context/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using EWalletV2.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EWalletV2.DataAccess.Context
+{
+    public class DataContext
+    {
+        public DbSet<UserEntity> User { get; set; }
+    }
+}
---
+++ Infrastructures/EWalletV2.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using EWalletV2.DataAccess.Context;
+using EWalletV2.Domain.Entities;
+using EWalletV2.Domain.Repoitories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EWalletV2.DataAccess.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public UserEntity GetUserByAccountNumber(string merchantAccNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserEntity GetUserByEmail(string email)
+        {
+            UserEntity user = _context.User.FirstOrDefault(x => x.Email == email.ToLower());
+            return user;
+        }
+    }
+}
