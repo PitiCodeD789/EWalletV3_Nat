6f3b7e235f9d8b0471499f0aadfc0ef7375e59e5
Pop_Chairat <Mr.Chairat_Smart@outlook.com>
2019-09-09 15:41:25 +0700

    Create transaction model and LoginPin model

---
+++ EWalletV2.Api.ViewModels/Auth/LoginPinCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EWalletV2.Api.ViewModels.Auth
+{
+   public class LoginPinCommand
+    {
+        public string Pin { get; set; }
+        public string Email { get; set; }
+
+    }
+}
