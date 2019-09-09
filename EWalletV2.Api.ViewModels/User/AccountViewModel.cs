464b816c573af58235b459cefee0c18e0fb27dff
Terawit Boonyasiri <terawit.boonyasiri@gmail.com>
2019-09-09 14:39:46 +0700

    AccountName
    AccountName

--- EWalletV2.Api.ViewModels/User/AccountViewModel.cs
+++ EWalletV2.Api.ViewModels/User/AccountViewModel.cs
@@ -6,7 +6,7 @@ namespace EWalletV2.Api.ViewModels.User
 {
     public class AccountViewModel
     {
-        public string Account { get; set; }
+        public string AccountName { get; set; }
         public decimal Balance { get; set; }
 
     }
