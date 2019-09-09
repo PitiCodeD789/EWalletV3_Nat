1723707d562cd3a37d4bf64d5b18c7d334eea163
Terawit Boonyasiri <terawit.boonyasiri@gmail.com>
2019-09-09 11:41:33 +0700

    conf
    conf

--- Applications/EWalletV2.Domain/Interfaces/IUserService.cs
+++ Applications/EWalletV2.Domain/Interfaces/IUserService.cs
@@ -8,10 +8,7 @@ namespace EWalletV2.Domain.Interfaces
     public interface IUserService
     {
         bool ExistingEmail(string email);
-<<<<<<< HEAD
         bool UpdateUser(UpdateUserDtoCommand userDto);
-=======
         string GetAccountNameByAccountNumber(string accountNumber);
->>>>>>> 33d3523f7c7c8b8a9cf7a478b01df41e8f7c588e
     }
 }
