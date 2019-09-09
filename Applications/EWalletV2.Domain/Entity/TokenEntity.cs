10f0f7700abe63716d4eb9d60583f9e587d6163e
prasert <sornprasit.pr@hotmail.com>
2019-09-09 16:53:57 +0700

    add token repository

--- Applications/EWalletV2.Domain/Entity/TokenEntity.cs
+++ Applications/EWalletV2.Domain/Entity/TokenEntity.cs
@@ -6,6 +6,7 @@ namespace EWalletV2.Domain.Entity
 {
    public class TokenEntity
     {
+        public int Id { get; set; }
         public string Email { get; internal set; }
         public string RefreshToken { get; internal set; }
     }
