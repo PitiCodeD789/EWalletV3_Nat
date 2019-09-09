29b4c1192ca2ab0cc72912e29a66c1a0aa6f5114
Terawit Boonyasiri <terawit.boonyasiri@gmail.com>
2019-09-09 14:37:33 +0700

    User Entities
    User Entities

--- EWalletV2.Api.ViewModels/User/AccountViewModel.cs
+++ EWalletV2.Api.ViewModels/User/AccountViewModel.cs
@@ -6,7 +6,7 @@ namespace EWalletV2.Api.ViewModels.User
 {
     public class AccountViewModel
     {
-        public string AccountName { get; set; }
+        public string Account { get; set; }
         public decimal Balance { get; set; }
 
     }
