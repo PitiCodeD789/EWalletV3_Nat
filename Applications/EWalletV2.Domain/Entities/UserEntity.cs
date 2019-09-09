f491dbce9cfd7737a0e36650c564502cca79eea5
Natthaset <natthaset2538@hotmail.com>
2019-09-09 15:22:25 +0700

    Add UserConfiguration

--- Applications/EWalletV2.Domain/Entities/UserEntity.cs
+++ Applications/EWalletV2.Domain/Entities/UserEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EWalletV2.Api.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,16 @@ namespace EWalletV2.Domain.Entities
 {
     public class UserEntity : BaseEntity
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string MobileNumber { get; set; }
+        public EW_Enumerations.EW_GenderEnum Gender { get; set; }
+        public string Email { get; set; }
+        public string Pin { get; set; }
+        public string Salt { get; internal set; }
+        public string Password { get; internal set; }
+        public decimal Balance { get; set; }
+        public string Account { get; set; }
     }
 }
--- Infrastructures/EWalletV2.DataAccess/Configurations/UserConfiguration.cs
+++ Infrastructures/EWalletV2.DataAccess/Configurations/UserConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using EWalletV2.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,38 @@ namespace EWalletV2.DataAccess.Configurations
     {
         public override void Configure(EntityTypeBuilder<UserEntity> e)
         {
-
+            e.Property(p => p.FirstName)
+                .HasColumnName("firstname")
+                .HasMaxLength(100);
+            e.Property(p => p.LastName)
+                .HasColumnName("lastname")
+                .HasMaxLength(100);
+            e.Property(p => p.BirthDate)
+                .HasColumnName("birthdate")
+                .HasDefaultValue("")
+                .HasMaxLength(100);
+            e.Property(p => p.MobileNumber)
+                .HasColumnName("mobile_number")
+                .HasMaxLength(100);
+            e.Property(p => p.Gender)
+                .HasColumnName("gender");
+            e.Property(p => p.Email)
+                .HasColumnName("email")
+                .HasMaxLength(100);
+            e.Property(p => p.Pin)
+                .HasColumnName("pin")
+                .HasMaxLength(100);
+            e.Property(p => p.Salt)
+                .HasColumnName("salt")
+                .HasMaxLength(100);
+            e.Property(p => p.Password)
+                .HasColumnName("password")
+                .HasMaxLength(100);
+            e.Property(p => p.Balance)
+                .HasColumnName("balance");
+            e.Property(p => p.Account)
+                .HasColumnName("account")
+                .HasMaxLength(100);
         }
     }
 }
