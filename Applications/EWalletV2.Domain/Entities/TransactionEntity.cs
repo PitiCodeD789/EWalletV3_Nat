1b5f3cbae8d5288e36d7124cd5e5a4ad31827ada
Songserm <songserm.js@gmail.com>
2019-09-09 16:44:28 +0700

    Add TransactionRepository

---
+++ Applications/EWalletV2.Domain/Entities/TransactionEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EWalletV2.Domain.Entities
+{
+    public class TransactionEntity : BaseEntity
+    {
+        public string TransactionReference { get; set; }
+        public int TransactionType { get; set; }
+        public int CustomerId { get; set; }
+        public int OtherId { get; set; }
+        public decimal Amount { get; set; }
+        public bool Status { get; set; }
+    }
+}
