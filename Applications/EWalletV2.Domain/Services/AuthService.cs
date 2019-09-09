5f196ed7800cfd4fa58b569617654adccd9d26a4
prasert <sornprasit.pr@hotmail.com>
2019-09-09 14:39:47 +0700

    create AuthSerice

---
+++ Applications/EWalletV2.Domain/Services/AuthService.cs
@@ -0,0 +1,59 @@
+﻿using EWalletV2.Domain.DtoModels.Auth;
+using EWalletV2.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EWalletV2.Domain.Services
+{
+    public class AuthService : IAuthService
+    {
+
+        
+
+        public bool CheckPin(string pin, string email)
+        {
+
+
+
+            return true;
+
+
+        }
+
+        public string GetRefreshToken(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LoginUserAndPassDto LoginWithUsernameAndPassword(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Logout(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Register(RegisterDtoCommand registerDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveOtp(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePin(string email, string oldPin, string newPin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateOtp(string email, string otp, string refNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
