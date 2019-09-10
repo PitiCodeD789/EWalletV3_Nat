34a1355176150997169f899dee508d465d73078c
prasert <sornprasit.pr@hotmail.com>
2019-09-10 20:11:35 +0700

    add authservice

---
+++ src/Client/EV.Service/Interfaces/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using EV.Service.Models;
+using EWalletV2.Api.ViewModels.Auth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EV.Service.Interfaces
+{
+   public interface IAuthService
+    {
+
+        Task<ResultServiceModel<CheckEmailViewModel>> SignIn(string email);
+        Task<ResultServiceModel<CheckOtpViewModel>> CheckOtp(string email, string otp, string refNumber);
+        Task<ResultServiceModel<RegisterViewModel>> Register(RegisterCommand register);
+        Task<ResultServiceModel<GetTokenByRefreshTokenViewModel>> GetTokenByRefreshToken(string email, string refreshToken);
+        Task<ResultServiceModel<LoginUserAndPassViewModel>> LoginUserAndPass(string username, string password);
+        Task<ResultServiceModel<DummyModel>> Logout(string email);
+    }
+}
