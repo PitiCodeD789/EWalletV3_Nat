e531004fe12e3706f847be0e1de3f55feac17bb0
tratam <tratam.jum@gmail.com>
2019-09-10 18:29:01 +0700

    create popup component

--- src/Client/Admin/EV.Admin/EV.Admin/App.xaml.cs
+++ src/Client/Admin/EV.Admin/EV.Admin/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EV.Admin.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace EV.Admin
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new ForgotPassword());
         }
 
         protected override void OnStart()
---
+++ src/Client/Admin/EV.Admin/EV.Admin/Components/EW_Popup.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EV.Admin.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EW_Popup : ContentView
+    {
+        public EW_Popup()
+        {
+            InitializeComponent();
+        }
+    }
+}
