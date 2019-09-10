fc90752823c7e973da4cd76e031ad2f43f34dc52
FreePM <622707130011@dpu.ac.th>
2019-09-10 18:37:41 +0700

    Add Base Url

---
+++ src/Client/EV.Service/Services/Constant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EV.Service.Services
+{
+    public static class Constant
+    {
+        public static string BaseUrl { get; set; } = "http://localhost:30000/api/";
+    }
+}
