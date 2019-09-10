881797d698bd1cb9cb676c1f74893257ec3f7bcf
Songserm <songserm.js@gmail.com>
2019-09-10 20:51:18 +0700

    Add UserService

---
+++ src/Client/EV.Service/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using EV.Service.Models;
+using EWalletV2.Api.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EV.Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ResultServiceModel<AccountViewModel>> GetUser(string email);
+        Task<ResultServiceModel<AccountViewModel>> GetBalance(string email);
+        Task<ResultServiceModel<AccountViewModel>> GetAccount(string accountNumber);
+        Task<ResultServiceModel<DummyModel>> UpdateUser(UpdateUserCommand updateUserCommand);
+    }
+}
