9e64ba3d3253f2a4714b1f0f8fd434747d3a3f49
prasert <sornprasit.pr@hotmail.com>
2019-09-11 12:42:33 +0700

    LoginViewModel

--- src/Client/Customer/EV.Customer/EV.Customer/ViewModels/LoginPageViewModel.cs
+++ src/Client/Customer/EV.Customer/EV.Customer/ViewModels/LoginPageViewModel.cs
@@ -1,9 +1,11 @@
-﻿using EV.Service.Interfaces;
+﻿using EV.Customer.Helper;
+using EV.Service.Interfaces;
 using EV.Service.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace EV.Customer.ViewModels
 {
@@ -13,9 +15,37 @@ namespace EV.Customer.ViewModels
         public LoginPageViewModel()
         {
             _authService = new AuthService();
+            SignInCommand = new Command(SignIn);
+        }
+
+        private async void SignIn()
+        {
+            bool isEmail = Unities.CheckEmailFormat(Email);
+            if(isEmail)
+            {
+                var result = await _authService.SignIn(Email);
+                if (result.IsError || result.Model == null)
+                {
+                    
+                    await Application.Current.MainPage.DisplayAlert("", "Error", "Ok");
+
+                }
+                else
+                {
+                    
+                    // TODO: valid or invalid Create viewModels supports Otp page and edit data
+                    await Application.Current.MainPage.Navigation.PushAsync(new Page());
+                }
+
+
+            }
+
+
+
         }
 
         public String Email { get; set; }
+        public Command SignInCommand { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
