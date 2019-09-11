7899d8fc1c87da9e63d84539e29ab27c2d53d1bf
prasert <sornprasit.pr@hotmail.com>
2019-09-11 10:11:09 +0700

    Create enum status

---
+++ src/Client/Customer/EV.Customer/EV.Customer/Helper/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EV.Customer.Helper
+{
+   public static class Status
+    {
+        public  enum LastPage
+        {
+            Login,
+            Register            
+        };
+        
+    }
+}
