0dfb68456821a4115a286f6c4ac0049acb902d3f
prasert <sornprasit.pr@hotmail.com>
2019-09-11 10:12:16 +0700

    Add ogin viewmodel

---
+++ src/Client/Customer/EV.Customer/EV.Customer/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using EV.Service.Interfaces;
+using EV.Service.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace EV.Customer.ViewModels
+{
+    public class LoginPageViewModel : INotifyPropertyChanged
+    {
+        private readonly IAuthService _authService;
+        public LoginPageViewModel()
+        {
+            _authService = new AuthService();
+        }
+
+        public String Email { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
