f92aa003ab7f75924649dae1d0b831029ebda132
keerati <san.keerati@gmail.com>
2019-09-11 14:49:56 +0700

    Add GenerateTopupViewModel

--- EWalletV2.Api.ViewModels/Transaction/GenerateTopupViewModel.cs
+++ EWalletV2.Api.ViewModels/Transaction/GenerateTopupViewModel.cs
@@ -7,5 +7,9 @@ namespace EWalletV2.Api.ViewModels.Transaction
     public class GenerateTopupViewModel
     {
         public string ReferenceNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CheckSum { get; set; }
+        public double Amount { get; set; }
     }
 }
