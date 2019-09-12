8a8dd18f6160141fd8c0c8758ca65b76a5d58ce5
Natthaset <natthaset2538@hotmail.com>
2019-09-12 16:26:09 +0700

    Add Receipt Page

---
+++ EWalletV2.Api.ViewModels/Transaction/SlipViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EWalletV2.Api.ViewModels.Transaction
+{
+    public class SlipViewModel
+    {
+        public string OtherName { get; set; }
+        public string OtherAccountNumber { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Reference { get; set; }
+        public EW_Enumerations.EW_TypeTransectionEnum Type { get; set; }
+    }
+}
