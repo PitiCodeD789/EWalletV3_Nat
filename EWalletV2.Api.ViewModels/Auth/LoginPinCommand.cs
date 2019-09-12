151ac945efc3d5a3b2d8c6f77179768ea330854f
Terawit Boonyasiri <terawit.boonyasiri@gmail.com>
2019-09-12 10:11:30 +0700

    Add AllowAnonymous and validate for pincontroller
    Add AllowAnonymousand validate for pincontroller

--- EWalletV2.Api.ViewModels/Auth/LoginPinCommand.cs
+++ EWalletV2.Api.ViewModels/Auth/LoginPinCommand.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EWalletV2.Api.ViewModels.Auth
 {
    public class LoginPinCommand
     {
+        [Required]
+        [RegularExpression(@"^(\d{6})$")]
         public string Pin { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
     }
