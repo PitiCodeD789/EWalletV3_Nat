b0b5bae142bb9419f6b0f73ee8650f1981ed757f
keerati <san.keerati@gmail.com>
2019-09-13 17:08:20 +0700

    add splashscreen

--- src/Client/Admin/EV.Admin/EV.Admin.Android/MainActivity.cs
+++ src/Client/Admin/EV.Admin/EV.Admin.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace EV.Admin.Droid
 {
-    [Activity(Label = "EV.Admin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "EV.Admin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
